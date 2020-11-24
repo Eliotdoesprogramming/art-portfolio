0f4f299da4289fb3085ec804b2654aef681c029e
Eliot Zubkoff <ezubkoff@C02CWQ7AML7H.local>
2020-11-24 16:06:10 -0600

    more commission changes

--- angular-art-portfolio/src/app/commission/commission.component.ts
+++ angular-art-portfolio/src/app/commission/commission.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { CommissionRequest } from '../commissionRequest';
+import { RequestService } from '../request.service';
 
 @Component({
   selector: 'app-commission',
@@ -7,9 +9,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CommissionComponent implements OnInit {
 
-  constructor() { }
+  constructor(private reqService:RequestService) { }
 
   ngOnInit(): void {
   }
-
+  submit(name:string,email:string,business:string,description:string){
+    let toSubmit:CommissionRequest={
+      id:0,  
+      name:name,
+      email:email,
+      description:description
+    }
+    this.reqService.addCommisionRequest(toSubmit).subscribe(id=>alert(`added request with id ${id}`));
+}
 }
