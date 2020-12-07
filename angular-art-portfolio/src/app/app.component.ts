d3ebb966e3e1dd04377c0880bd680c22e348ae61
Eliot Zubkoff <ezubkoff@C02CWQ7AML7H.local>
2020-12-07 08:11:50 -0600

    configuration

--- angular-art-portfolio/src/app/app.component.ts
+++ angular-art-portfolio/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AuthService } from './auth.service';
+import { TokenStorageService } from './token-storage.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  userName:string ="";
   title = "";
+  constructor(private token:TokenStorageService){}
+  getUserName(){
+    this.token.getUser()
+  }
 }
